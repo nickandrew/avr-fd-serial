c3d37569ce3716613abd8afc8671ee12ac143a76
Nick Andrew <nick@nick-andrew.net>
2010-12-19 01:21:16 +1100

    This one demonstrates the PWM mode difference well
    enable PWM1B to cause changes to OCR1B to not take effect
    until the next timer loop.

--- test-serial0.c
+++ test-serial0.c
@@ -54,6 +54,16 @@ volatile uint8_t a_counter[NR_MARKERS];
 volatile uint8_t a_index = 0;
 volatile uint8_t a_max = NR_MARKERS;
 
+volatile uint8_t cycle_64 = 0;
+volatile uint8_t cycle_74 = 0;
+volatile uint8_t cycle_94 = 0;
+volatile uint8_t cycle_10 = 0;
+volatile uint8_t tcnt_64 = 0;
+volatile uint8_t tcnt_74 = 0;
+volatile uint8_t tcnt_94 = 0;
+volatile uint8_t tcnt_10 = 0;
+uint8_t done = 0;
+
 char *newline = "\n";
 
 int main(void)
@@ -70,14 +80,17 @@ int main(void)
 	uint8_t ctc_mode = 1<<CTC1;
 	uint8_t com_mode = 0<<COM1A1 | 0<<COM1A0;
 	uint8_t prescaler = 1<<CS13 | 1<<CS12 | 1<<CS11 | 1<<CS10;
+	uint8_t pwm_mode = 0<<PWM1A;
 
 	TCNT1 = 0;
-	OCR1A = 64;
-	OCR1B = 128;
+	OCR1A = 32;
+	OCR1B = 64;
 	OCR1C = 207;
 	TIMSK |= 1<<OCIE1A | 1<<OCIE1B;
 	DDRB |= 1<<PORTB3;
 	PORTB |= 1<<PORTB3;
+	// TCCR1 |= 1<<PWM1A;
+	// GTCCR |= 1<<PWM1B;
 	TCCR1 = ctc_mode | com_mode | prescaler;
 	GIMSK |= 1<<INT0;
 
@@ -88,6 +101,15 @@ int main(void)
 	uint8_t old_tcnt1 = 0;
 
 	writeString("Starting\n");
+	writeString("Really starting\n");
+
+//	while (1) {
+//		serial0_send('<');
+//		serial0_send('<');
+//		serial0_send('<');
+//		serial0_delay(1000);
+//		serial0_send('>');
+//	}
 
 	while (1) {
 		loops ++;
@@ -95,37 +117,24 @@ int main(void)
 
 		if (tcnt1 < old_tcnt1) {
 			cycle_count ++;
-			if (a_index < a_max) {
-				a_state[a_index] = 4;
-				a_cycle[a_index] = cycle_count;
-				a_ocr1b[a_index] = 0;
-				a_counter[a_index] = tcnt1;
-				a_index ++;
-			}
 		}
 
 		old_tcnt1 = tcnt1;
 
-		if (a_index > 0) {
-			writeString("T1:");
-			uint8_t i;
-			for (i = 0; i < a_index; ++i) {
-				write8(i);
-				serial0_send(':');
-				write8(a_state[i]);
-				serial0_send(':');
-				write8(a_cycle[i]);
-				serial0_send(':');
-				write8(a_ocr1b[i]);
-				serial0_send(':');
-				write8(a_counter[i]);
-				serial0_send('>');
-			}
-			a_index = 0;
+		if (cycle_10 && !done) {
+			writeString("Done: <");
+			write8(cycle_64);
+			write8(tcnt_64);
+			write8(cycle_74);
+			write8(tcnt_74);
+			write8(cycle_94);
+			write8(tcnt_94);
+			write8(cycle_10);
+			write8(tcnt_10);
+			serial0_send('>');
 			serial0_send('\n');
-
-			// TCCR1 &= ~prescaler;
-			// while (1) { }
+			done = 1;
+			writeString("Finito\n");
 		}
 	}
 }
@@ -134,16 +143,7 @@ int main(void)
 
 ISR(TIMER1_COMPA_vect)
 {
-	uint8_t tcnt = TCNT1;
-	uint8_t cycles = cycle_count;
-
-	if (a_index < a_max) {
-		a_state[a_index] = 1;
-		a_cycle[a_index] = cycles;
-		a_ocr1b[a_index] = OCR1A;
-		a_counter[a_index] = tcnt;
-		a_index ++;
-	}
+	// Do nothing
 }
 
 // Interrupt routine for timer1, TCCR1A, rx bits
@@ -152,13 +152,27 @@ ISR(TIMER1_COMPB_vect)
 {
 	uint8_t tcnt = TCNT1;
 	uint8_t cycles = cycle_count;
+	uint8_t ocr1b = OCR1B;
 
-	if (a_index < a_max) {
-		a_state[a_index] = 2;
-		a_cycle[a_index] = cycles;
-		a_ocr1b[a_index] = OCR1B;
-		a_counter[a_index] = tcnt;
-		a_index ++;
+	if (ocr1b == 64) {
+		cycle_64 = cycles;
+		tcnt_64 = tcnt;
+		OCR1B = 74;
+	}
+	else if (ocr1b == 74) {
+		cycle_74 = cycles;
+		tcnt_74 = tcnt;
+		OCR1B = 94;
+	}
+	else if (ocr1b == 94) {
+		cycle_94 = cycles;
+		tcnt_94 = tcnt;
+		OCR1B = 10;
+	}
+	else if (ocr1b == 10) {
+		cycle_10 = cycles;
+		tcnt_10 = tcnt;
+		OCR1B = 22;
 	}
 }
 
@@ -168,12 +182,5 @@ ISR(INT0_vect) {
 	// This will cause a timer interrupt half a bit time later
 	uint8_t tcnt1 = TCNT1;
 	OCR1B = (tcnt1 + 90) % 208;
-
-	if (a_index < a_max) {
-		a_state[a_index] = 3;
-		a_cycle[a_index] = cycle_count;
-		a_ocr1b[a_index] = OCR1B;
-		a_counter[a_index] = tcnt1;
-		a_index ++;
-	}
+	count_int0 ++;
 }
